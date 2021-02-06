29ebb4edffd9ebfc93d41af78ed7a26286a64fd3
ThomasTW935 <thomastw935@gmail.com>
2021-02-06 16:20:05 +0800

    Added script for todo status selection

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+let todos = [
+  {
+    todo: 'Run 5km',
+    status: 'active'
+  },
+  {
+    todo: 'Bike 10km',
+    status: 'completed'
+  },
+  {
+    todo: 'Bike 50km',
+    status: 'active'
+  }
+
+]
+
 init()
 
 function init() {
-  let todos = [
-    {
-      todo: 'Run 5km',
-      status: 0
-    },
-    {
-      todo: 'Bike 10km',
-      status: 1
-    },
-    {
-      todo: 'Bike 50km',
-      status: 0
-    }
 
-  ]
   renderTodos(todos)
+  setStatusEvents()
 }
 function renderTodos(todos) {
   let todoList = document.querySelector('.todo__list')
@@ -43,7 +46,7 @@ function renderTodo(todo) {
   let img = document.createElement('img')
 
   checkBox.type = 'checkbox'
-  checkBox.checked = todo.status
+  checkBox.checked = (todo.status == 'completed') ? 1 : 0
 
   img.src = './images/icon-cross.svg'
   close.append(img)
@@ -51,6 +54,8 @@ function renderTodo(todo) {
   checkBoxLabel.append(checkBox)
   checkBoxLabel.append(checkBoxSpan)
 
+  let className = (todo.status == 'completed') ? 'todo__item--active' : 'todo__item'
+  item.classList.add(className)
   item.append(checkBoxLabel)
   item.append(p)
   item.append(close)
@@ -60,4 +65,22 @@ function renderTodo(todo) {
 function filterTodos(todos, statusSelected) {
   let newTodos = todos.filter(({ status }) => status === statusSelected)
   return newTodos
+}
+function setStatusEvents() {
+  let statuses = document.querySelectorAll('.todo__status label input')
+  statuses.forEach(status => {
+    status.addEventListener('change', () => {
+      let newTodos = (status.value != 'all') ? filterTodos(todos, status.value) : todos
+      removeListItems()
+      renderTodos(newTodos)
+    })
+  })
+}
+function removeListItems() {
+  let todoList = document.querySelector('.todo__list')
+  let count = todoList.childNodes.length
+
+  for (let i = count - 1; i >= 0; i--) {
+    todoList.childNodes[i].remove()
+  }
 }
